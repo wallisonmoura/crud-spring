36df52d6de4180eac32d8815d3aee7e8fc73ce94
wallisonmoura <wallison_cedaspy@hotmail.com>
2024-10-17 23:46:44 -0300

    feat: exceptions e controller advice

--- src/main/java/com/api/controller/CourseController.java
+++ src/main/java/com/api/controller/CourseController.java
@@ -3,7 +3,6 @@ package com.api.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,10 +39,8 @@ public class CourseController {
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id) {
-    return courseService.findById(id)
-        .map(recordFound -> ResponseEntity.ok(recordFound))
-        .orElse(ResponseEntity.notFound().build());
+  public Course findById(@PathVariable @NotNull @Positive Long id) {
+    return courseService.findById(id);
   }
 
   @PostMapping
@@ -53,18 +50,14 @@ public class CourseController {
   }
 
   @PutMapping("/{id}")
-  public ResponseEntity<Course> update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid Course course) {
-    return courseService.update(id, course)
-        .map(recordFound -> ResponseEntity.ok().body(recordFound))
-        .orElse(ResponseEntity.notFound().build());
+  public Course update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid Course course) {
+    return courseService.update(id, course);
   }
 
   @DeleteMapping("/{id}")
-  public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id) {
-    if (courseService.delete(id)) {
-      return ResponseEntity.noContent().<Void>build();
-    }
-    return ResponseEntity.notFound().build();
+  @ResponseStatus(code = HttpStatus.NO_CONTENT)
+  public void delete(@PathVariable @NotNull @Positive Long id) {
+    courseService.delete(id);
   }
 
 }
--- src/main/java/com/api/service/CourseService.java
+++ src/main/java/com/api/service/CourseService.java
@@ -1,12 +1,12 @@
 package com.api.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.api.exception.RecordNotFoundException;
 import com.api.model.Course;
 import com.api.repository.CourseRepository;
 
@@ -28,30 +28,26 @@ public class CourseService {
     return courseRepository.findAll();
   }
 
-  public Optional<Course> findById(@PathVariable @NotNull @Positive Long id) {
-    return courseRepository.findById(id);
+  public Course findById(@PathVariable @NotNull @Positive Long id) {
+    return courseRepository.findById(id).orElseThrow(() -> new RecordNotFoundException(id));
   }
 
   public Course create(@Valid Course course) {
     return courseRepository.save(course);
   }
 
-  public Optional<Course> update(@NotNull @Positive Long id, @Valid Course course) {
+  public Course update(@NotNull @Positive Long id, @Valid Course course) {
     return courseRepository.findById(id)
         .map(recordFound -> {
           recordFound.setName(course.getName());
           recordFound.setCategory(course.getCategory());
           return courseRepository.save(recordFound);
-        });
+        }).orElseThrow(() -> new RecordNotFoundException(id));
   }
 
-  public boolean delete(@NotNull @Positive Long id) {
-    return courseRepository.findById(id)
-        .map(recordFound -> {
-          courseRepository.deleteById(id);
-          return true;
-        })
-        .orElse(false);
+  public void delete(@NotNull @Positive Long id) {
+    courseRepository.delete(courseRepository.findById(id)
+        .orElseThrow(() -> new RecordNotFoundException(id)));
   }
 
 }
