b7622e7666c944fa6e4fe2e53e12708bebe64a2b
wallisonmoura <wallison_cedaspy@hotmail.com>
2024-10-21 23:29:35 -0300

    feat: enum validation

--- src/main/java/com/api/dto/CourseDTO.java
+++ src/main/java/com/api/dto/CourseDTO.java
@@ -4,18 +4,19 @@ import java.util.List;
 
 import org.hibernate.validator.constraints.Length;
 
+import com.api.enums.Category;
+import com.api.enums.validation.ValueOfEnum;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 
 public record CourseDTO(
-    @JsonProperty("_id") Long id,
-    @NotBlank @NotNull @Length(min = 4, max = 100) String name,
-    @NotNull @Length(max = 10) @Pattern(regexp = "Back-end|Front-end") String category,
-    @NotNull @NotEmpty @Valid List<LessonDTO> lessons) {
+        @JsonProperty("_id") Long id,
+        @NotBlank @NotNull @Length(min = 4, max = 100) String name,
+        @NotNull @Length(max = 10) @ValueOfEnum(enumClass = Category.class) String category,
+        @NotNull @NotEmpty @Valid List<LessonDTO> lessons) {
 
 }
